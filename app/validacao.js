1a5a1c1a1dd57125e52c6525cb5b6eaefc3878dc
Mariana Batista <100529895+Mariana-Batista@users.noreply.github.com>
2023-09-12 09:39:08 -0300

    Jogo acerte o número secreto
    Jogo desenvolvido para testes de habilidade com eventos em JS.

---
+++ app/validacao.js
@@ -0,0 +1,43 @@
+function verificarSeOChutePossuiUmValorValido(chute) {
+    const numero = +chute
+
+    if (chuteForInvalido(numero)) {
+        elementoChute.innerHTML += '<div>Valor inválido</div>'
+        return
+    }
+
+    if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
+        elementoChute.innerHTML += `<div>Valor inválido: Fale um número entrez ${menorValor} e 
+        ${maiorValor}</div>`
+        return
+    }
+
+    if (numero === numeroSecreto) {
+        document.body.innerHTML = `
+            <h2>Você acertou!!!</h2>
+            <h3>O número secreto é ${numeroSecreto}</h3>
+            <button id="jogar-novamente"" class="btn-jogar">Jogar novamente</button>
+        `
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+        <div>O número secreto é menor <i class="fa-regular fa-square-caret-down fa-bounce"></i></div>`
+    } else {
+        elementoChute.innerHTML += `
+        <div>O número secreto é maior <i class="fa-regular fa-square-caret-up fa-bounce"></i></div>`
+    }
+        
+}
+
+function chuteForInvalido(numero) {
+    return Number.isNaN(numero)
+}
+
+function numeroForMaiorOuMenorQueOValorPermitido(numero) {
+    return numero > maiorValor || numero < menorValor 
+}
+
+document.body.addEventListener('click', e => {
+    if (e.target.id == 'jogar-novamente') {
+        window.location.reload()
+    }
+})
